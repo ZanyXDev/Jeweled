f76d9cad37be5a8b73f546ed58b7fc4fbddc0cda
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2022-11-13 15:22:04 +0300

    add build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+DIR="build-2022"
+QMAKE="/home/zanyxdev/Qt/5.15.2/gcc_64/bin/qmake"
+APP="FreeJeweled"
+
+if [ -d $DIR ] ; then
+  if [ "$1" = yes ] ; then
+     echo "Make clean"
+    /usr/bin/make -j4 -C $DIR clean 
+  fi
+else 
+  mkdir $DIR			
+fi
+
+cd $DIR
+
+rm $APP
+
+$QMAKE ../jeweled.pro -spec linux-g++ CONFIG+=debug CONFIG+=qml_debug && /usr/bin/make -j4 qmake_all && /usr/bin/make -j4
+
+if [ -e $APP ]; then
+  echo "Success build"
+  ls -lah $APP
+fi
