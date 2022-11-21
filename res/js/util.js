cb472ed2a9f523ad8fe1a847e89d8e5760fc0df2
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2022-11-21 12:48:05 +0300

    recalc brg item position for centred

--- res/js/util.js
+++ res/js/util.js
@@ -5,9 +5,7 @@ function getXFromIndex(index, m_size, m_offset) {
         return -1
     }
     var m_col = index % 8
-    var x = m_col * (m_size + m_offset)
-    x += m_offset
-    //console.log(`getXFromIndex[${index}] is [x:${x}] position, size:${m_size}`)
+    var x = (m_col * (m_size + (2 * m_offset))) + 5 * m_offset
     return x
 }
 
@@ -15,24 +13,25 @@ function getYFromIndex(index, m_size, m_offset) {
     if (index < 0) {
         return -1
     }
+
     var m_col = index % 8
     var m_row = (index > 7) ? ((index - m_col) / 8) : 0
-    var y = m_row * (m_size + m_offset)
+    var y = (m_row * (m_size + (2 * m_offset))) + 5 * m_offset
 
-    y += m_offset
+    //console.log(`getYFromIndex ${index}  is [y:${y}] position`)
     return y
 }
 
 function fillBgrModel(m_model, m_count, m_size, m_dp) {
-
+    m_model.clear()
     for (var index = 0; index < m_count; index++) {
-
         m_model.append({
                            "x": 0,
                            "y": 0,
-                           "new_x": getXFromIndex(index, m_size * m_dp, m_dp),
-                           "new_y": getYFromIndex(index, m_size * m_dp, m_dp),
-                           "visible": true
+                           "new_x": getXFromIndex(index, m_size, m_dp),
+                           "new_y": getYFromIndex(index, m_size, m_dp),
+                           "visible": true,
+                           "m_size": m_size
                        })
     }
 }
