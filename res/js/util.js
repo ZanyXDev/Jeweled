cea66528e9ee762640ae17357e6d954404fee5a4
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2022-11-21 12:48:37 +0300

    add func create and move to start posiotion bgr item

--- res/js/util.js
+++ res/js/util.js
@@ -35,3 +35,29 @@ function fillBgrModel(m_model, m_count, m_size, m_dp) {
                        })
     }
 }
+
+function createBackgroundTile(m_parent, m_item, m_model) {
+    var m_size = m_model.get(0).m_size
+    for (var index = 0; index < m_model.count; index++) {
+
+        var m_bgr = m_item.createObject(m_parent, {
+                                            "x": m_model.get(index).x,
+                                            "y": m_model.get(index).y,
+                                            "height": m_size,
+                                            "width": m_size,
+                                            "visible": true
+                                        })
+    }
+}
+
+function moveBackgroundTile(m_model) {
+    // console.log("moveBackgroundTile() begin")
+    for (var index = 0; index < m_model.count; index++) {
+
+        //console.log(`Idx ${index} y:${m_model.get(index).y}`)
+        m_model.setProperty(index, "x", m_model.get(index).new_x)
+
+        m_model.setProperty(index, "y", m_model.get(index).new_y)
+        // console.log(`Idx ${index} y2:${m_model.get(index).y}`)
+    }
+}
